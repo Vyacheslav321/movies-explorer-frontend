d26c57acfd35fd60909a2ed7a888dec2c21597ee
Vyacheslav321 <apxsale@yandex.ru>
2023-01-07 18:55:57 +0300

    fix: добавить регистронезависимый поиск

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,10 +1,26 @@
 import { SHORTMOVIE_DURATION } from "./Constants";
 
 // обработчик поиска
+// function handleSearch(movies, searchKeyword) {
+//   let searchResult = [];
+//   movies.forEach((movie) => {
+//     if (movie.nameRU.indexOf(searchKeyword) !== -1) {
+//       return searchResult.push(movie);
+//     }
+//   });
+//   return searchResult;
+// }
+
 function handleSearch(movies, searchKeyword) {
   let searchResult = [];
+  const searchKeywordNormalize = searchKeyword.toLowerCase().trim();
   movies.forEach((movie) => {
-    if (movie.nameRU.indexOf(searchKeyword) !== -1) {
+    const movieRuNormalize = String(movie.nameRU).toLowerCase().trim();
+    const movieEnNormalize = String(movie.nameEN).toLowerCase().trim();
+    if (
+      movieRuNormalize.indexOf(searchKeywordNormalize) !== -1 ||
+      movieEnNormalize.indexOf(searchKeywordNormalize) !== -1
+    ) {
       return searchResult.push(movie);
     }
   });
@@ -18,7 +34,7 @@ function handleFilterShortMovies(movies) {
 function filterMovies(movies, searchKeyword, shortMoviesChecked) {
   const searchResult = handleSearch(movies, searchKeyword);
   if (shortMoviesChecked) {
-    console.log(shortMoviesChecked)
+    console.log(shortMoviesChecked);
     return handleFilterShortMovies(searchResult);
   } else {
     return searchResult;
